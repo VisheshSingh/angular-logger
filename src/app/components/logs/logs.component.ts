70f5ec649935a51cb4b65148fe184e3bb2afb65c
Vishesh <visheshsingh16@gmail.com>
2018-10-07 13:11:33 -0400

    Building the UI and models for interface

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Log } from "../../models/Log";
 
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+  selector: "app-logs",
+  templateUrl: "./logs.component.html",
+  styleUrls: ["./logs.component.css"]
 })
 export class LogsComponent implements OnInit {
+  logs: Log[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.logs = [
+      { id: 1, text: "Generated components", date: "12/27/2017 09:33:12" },
+      { id: 2, text: "Added bootstrap", date: "12/28/2017 12:45:10" },
+      { id: 3, text: "Log form component", date: "12/20/2017 02:12:56" }
+    ];
   }
-
 }
