1e0d3cb2cfb138c8cf9f55d48ce52a0c637c0399
Vishesh <visheshsingh16@gmail.com>
2018-10-07 14:40:06 -0400

    conditional rendering and clean up

--- src/app/services/logs.service.ts
+++ src/app/services/logs.service.ts
@@ -19,21 +19,21 @@ export class LogsService {
 
   constructor() {
     this.logs = [
-      {
-        id: "1",
-        text: "Generated components",
-        date: new Date("12/27/2017 09:33:12")
-      },
-      {
-        id: "2",
-        text: "Added bootstrap",
-        date: new Date("12/28/2017 12:45:10")
-      },
-      {
-        id: "3",
-        text: "Log form component",
-        date: new Date("12/20/2017 02:12:56")
-      }
+      // {
+      //   id: "1",
+      //   text: "Generated components",
+      //   date: new Date("12/27/2017 09:33:12")
+      // },
+      // {
+      //   id: "2",
+      //   text: "Added bootstrap",
+      //   date: new Date("12/28/2017 12:45:10")
+      // },
+      // {
+      //   id: "3",
+      //   text: "Log form component",
+      //   date: new Date("12/20/2017 02:12:56")
+      // }
     ];
   }
 
